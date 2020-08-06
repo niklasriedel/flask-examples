dff52fb6e3f2094e33901a2c7328c9acdc81b013
nriedel <niklas.riedel.le@gmail.com>
2020-08-06 11:14:20 +0200

    neues Feld hinzugefügt -> Altersabfrage hinzugefügt

--- 03-forms/hello.py
+++ 03-forms/hello.py
@@ -4,9 +4,11 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     name = None
+    alter = ''
     if request.method == 'POST' and 'name' in request.form:
         name = request.form['name']
-    return render_template('index.html', name=name)
+        alter = request.form['alter']
+    return render_template('index.html', name=name, alter=alter)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
