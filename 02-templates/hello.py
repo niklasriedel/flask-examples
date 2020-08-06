956b22d7d4b8132ce27f7b12fd2808c5a20e61a2
nriedel <niklas.riedel.le@gmail.com>
2020-08-06 10:38:26 +0200

    2 neue Funktionen hinzu gefügt und templates angewendet

--- 02-templates/hello.py
+++ 02-templates/hello.py
@@ -5,9 +5,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+@app.route('/greet_user/<name>')
+def greet_user(name):
+    return render_template('greet_user.html', name=name)
+
+@app.route('/bye_user/<name>')
+def bye_user(name):
+    return render_template('bye_user.html', name=name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
