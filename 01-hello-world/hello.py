c66c33cd1eac74c0a294046a310334b43ee346ca
nriedel <niklas.riedel.le@gmail.com>
2020-08-06 10:23:11 +0200

    2 neue Funktionen hinzu gefügt

--- 01-hello-world/hello.py
+++ 01-hello-world/hello.py
@@ -3,12 +3,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello, World!</h1>"
+    return "<h1>Hallo, Welt!</h1>"
 
-@app.route('/user/<name>')
-def user(name):
-	return '<h1>Hello, {0}!</h1>'.format(name)
+@app.route('/greet_user/<name>')
+def greet_user(name):
+	return '<h1>Hallo, {0}!</h1>'.format(name)
+
+@app.route('/bye_user/<name>')
+def bye_user(name):
+	return '<h1>Tschau, {0}!</h1>'.format(name)
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+
+
 
